8688e2bdf198f1be140b86b44a5576bde8903e8e
Marcos Magaña <imarcosmagana@gmail.com>
2024-04-02 13:16:28 -0600

    tkinker - formularios 2 - checkbox

--- 21-tkinter/11-formularios2.py
+++ 21-tkinter/11-formularios2.py
@@ -13,14 +13,44 @@ encabezado.config(
     bg="green",
     font=("Consolas",20)
 )
+
 encabezado.grid(row=0, column=0, columnspan=5, sticky=W)
 
 
 # Botones check
-Label(ventana, text="A que te dedicas? ").grid(row=1, column=0)
-Checkbutton(ventana, text="Desarrollo Web").grid(row=2, column=0)
-Checkbutton(ventana, text="Desarrollo Movil").grid(row=3, column=0)
+def mostrarProfesion():
+    texto = ""
+    if web.get():
+        texto += "Desarrollo web "
 
+    if movil.get():
+        if web.get():
+            texto += "y Desarrollo movil"
+        else:
+            texto += "Desarrollo movil "
+    
+    mostrar.config(text=texto, bg="green", fg="white")
+
+web = IntVar()
+movil = IntVar()
+
+
+Label(ventana, text="A que te dedicas? ").grid(row=1, column=0)
+Checkbutton(ventana, text="Desarrollo Web",
+            variable=web,
+            onvalue=1,
+            offvalue=0,
+            command=mostrarProfesion
+).grid(row=2, column=0)
+Checkbutton(ventana, text="Desarrollo Movil",
+            variable=movil,
+            onvalue=1,
+            offvalue=0,
+            command=mostrarProfesion
+).grid(row=3, column=0)
+
+mostrar = Label(ventana)
+mostrar.grid(row=4, column=0)
 
 
 ventana.mainloop()
