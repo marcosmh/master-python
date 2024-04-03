e67e54905b57d0f6959820041023f5060abba2d7
Marcos Magaña <imarcosmagana@gmail.com>
2024-04-02 20:16:40 -0600

    proyecto - pantallas - form1

--- 21-tkinter/13-proyecto.py
+++ 21-tkinter/13-proyecto.py
@@ -40,14 +40,32 @@ def home():
 
 
 def add():
+    # Encabezado
     add_label.config(
         fg="white",
         bg="black",
         font=("Arial", 30),
-        padx=320,
+        padx=360,
         pady=20
     )
-    add_label.grid(row=0, column=0)
+    add_label.grid(row=0, column=0, columnspan=10)
+    
+    # Campos del formulario Añadir
+    add_name_label.grid(row=1, column=0, padx=6, pady=6, sticky=E)
+    add_name_entry.grid(row=1, column=1, padx=6, pady=6, sticky=W)
+
+    add_price_label.grid(row=2, column=0, padx=6, pady=6, sticky=E)
+    add_price_entry.grid(row=2, column=1, padx=6, pady=6, sticky=W)
+
+    add_description_label.grid(row=3, column=0, padx=6, pady=6, sticky=E)
+    add_description_entry.grid(row=3, column=1, padx=6, pady=6, sticky=W)
+    #add_description_entry.config(width=30,font=("Consolas", 12),padx=16,pady=16)
+
+    add_separator.grid(row=4)
+
+    boton.grid(row=5, column=1, sticky=NW)
+    boton.config(padx=16, pady=6, bg="green", fg="red")
+    
     home_label.grid_remove()
     info_label.grid_remove()
     data_label.grid_remove()
@@ -67,18 +85,42 @@ def info():
     home_label.grid_remove()
     add_label.grid_remove()
     
-    
 
-# Definir pantallas
+# Variables
+name_data = StringVar()
+price_data = StringVar()    
+description_data = StringVar()
+
+# Definir pantalla Inicio
 home_label = Label(ventana, text="Inicio")
+
+# Definir pantalla Añadir
 add_label = Label(ventana, text="Añadir")
+
+# Campos del formulario
+add_name_label = Label(ventana, text="Nombre: ")
+add_name_entry = Entry(ventana,textvariable=name_data)
+
+add_price_label = Label(ventana, text="Precio: ")
+add_price_entry = Entry(ventana,textvariable=price_data)
+
+add_description_label = Label(ventana, text="Descripción: ")
+add_description_entry = Entry(ventana,textvariable=description_data)
+
+add_separator = Label(ventana)
+
+boton = Button(ventana, text="Guardar")
+
+
+
+# Definir pantalla Información
 info_label = Label(ventana, text="Información")
 data_label = Label(ventana, text="Creado por Marcos Magaña -  2024")
 
 # cargar pantallas
-home()
+#home()
 add()
-info()
+#info()
 
 # Menú superior
 menu_superior = Menu(ventana)
