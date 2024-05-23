236015b43a098f50bb103c6dcceeb63dd53de0b1
Marcos Magaña <imarcosmagana@gmail.com>
2024-05-22 21:38:50 -0600

    personalizar panel de administracion

--- 22-django/proyecto_django/pages/admin.py
+++ 22-django/proyecto_django/pages/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 from .models import Page
 
+#configuracion extra
+class PageAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at','update_at')
+    search_fields = ('title','content')
+    list_filter = ('visible',)
+    list_display = ('title','visible','created_at')
+    ordering = ('created_at',)
+
+
+
 # Register your models here.
-admin.site.register(Page)
+admin.site.register(Page, PageAdmin)
 
 #configuar panel
 title = "Panel Control de Páginas"
