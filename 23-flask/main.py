4e2dfc1ca49bf8bb66d36b07aa852657e6e72dde
Marcos Magaña <imarcosmagana@gmail.com>
2024-06-20 22:06:13 -0600

    Flask - plantillas y templates

--- 23-flask/main.py
+++ 23-flask/main.py
@@ -1,10 +1,29 @@
 from flask import Flask,redirect, url_for, render_template
+from datetime import datetime
 
 app = Flask(__name__)
 
+# Context processors
+@app.context_processor
+def date_now():
+    return {
+        'now': datetime.utcnow()
+    }
+
+# Endpoints
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    edad = 18
+    personas = ['Marcos','Victor','Francisco']
+
+    return render_template('index.html',
+                           edad = edad,
+                           dato1='Valor1',
+                           dato2='Valor2',
+                           lista=['uno','dos','tres'],
+                           personas=personas
+                           )
 
 @app.route('/informacion')
 @app.route('/informacion/<string:nombre>')
@@ -25,11 +44,11 @@ def contacto(redireccion = None):
     if redireccion is not None:
         return redirect(url_for('lenguajes'))
 
-    return "<h1>Página de Contacto</h1>"
+    return render_template('contacto.html')
 
 @app.route('/lenguajes-de-programacion')
 def lenguajes():
-    return "<h1>Páginas de Lenguajes de Programación"
+    return render_template('lenguajes.html')
 
 
 if __name__ == '__main__':
