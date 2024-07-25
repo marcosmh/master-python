d2de2d8ef7edc21686f2f6bbc1958cc4e612402b
Marcos Magaña <imarcosmagana@gmail.com>
2024-07-24 19:48:31 -0600

    Flask - insertar daos en mysql

--- 23-flask/main.py
+++ 23-flask/main.py
@@ -1,7 +1,18 @@
 from flask import Flask,redirect, url_for, render_template
 from datetime import datetime
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+
+#Conexion DB
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'admindba'
+app.config['MYSQL_DB'] = 'proyectoflask'
+
+mysql = MySQL(app)
+
+
 
 # Context processors
 @app.context_processor
@@ -50,6 +61,14 @@ def contacto(redireccion = None):
 def lenguajes():
     return render_template('lenguajes.html')
 
+
+@app.route('/insertar-coche')
+def insertar_coche():
+    cursor = mysql.connection.cursor()
+    cursor.execute(f'INSERT INTO coches VALUES (NULL,"Lambo","Gallardo",13.40,"CDMX") ')
+    cursor.connection.commit()
+    
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
